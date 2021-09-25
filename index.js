dc992ebea5d4409ee00e6d392e995c15354aa142
Holcim DEV <holcim.dev.user@gmail.com>
2021-09-25 10:54:52 -0500

    1.0.0

--- index.js
+++ index.js
@@ -100,8 +100,8 @@ app.get(process.env.iisVirtualPath+'status', function (req, res) {
         ,connectionPool_eventsCount: connectionPool._eventsCount
         ,connectionPool_db: connectionPool.config.database
         ,connectionPool_connected: connectionPool._connected
-        ,connectionPool_poolMax: connectionPool.pool.max
-        ,connectionPool_poolUsed: connectionPool.pool.used
+        //,connectionPool_poolMax: connectionPool.pool.max
+        //,connectionPool_poolUsed: connectionPool.pool.used
         
     }
     res.send(JSON.stringify(respuesta));
@@ -2228,6 +2228,75 @@ app.post(process.env.iisVirtualPath+'spNotificationsUpdate', veryfyToken, functi
     })
 })
 //#endregion NOTIFICATIONS
+
+////#region CHOFER
+app.get(process.env.iisVirtualPath+'spChoferesSelectEdit', veryfyToken, function(req, res) {
+    let start = new Date()
+    jwt.verify(req.token, process.env.secretEncryptionJWT, (jwtError, authData) => {
+        if(jwtError){
+            logToFile("JWT Error:")
+            logToFile(jwtError)
+            res.status(403).send(jwtError);
+        }else{
+            new sql.Request(connectionPool)
+            .input('userCode', sql.Int, req.query.userCode )
+            .input('userCompany', sql.Int, req.query.userCompany )
+            .input('userLanguage', sql.VarChar(50), req.query.userLanguage )
+            .input('row_id', sql.Int, req.query.row_id )
+            .input('editMode', req.query.editMode )//.input('editMode', sql.Bit, req.query.editMode )
+            .execute('spChoferesSelectEdit', (err, result) => {
+                logToFile("Request:  " + req.originalUrl)
+                logToFile("Perf spChoferesSelectEdit:  " + ((new Date() - start) / 1000) + ' secs' )
+                if(err){
+                    logToFile("DB Error:  " + err.procName)
+                    logToFile("Error:  " + JSON.stringify(err.originalError.info))
+                    res.status(400).send(err.originalError);
+                    return;
+                }
+                res.setHeader('content-type', 'application/json');
+                res.status(200).send(result.recordset);
+            })
+        }
+    })
+})
+app.post(process.env.iisVirtualPath+'spChoferesUpdate', veryfyToken, function(req, res) {
+    let start = new Date()
+    jwt.verify(req.token, process.env.secretEncryptionJWT, (jwtError, authData) => {
+        if(jwtError){
+            logToFile("JWT Error:")
+            logToFile(jwtError)
+            res.status(403).send(jwtError);
+        }else{
+            try{
+                new sql.Request(connectionPool)
+                .input('userCode', sql.Int, req.body.userCode )
+                .input('userCompany', sql.Int, req.body.userCompany )
+                .input('row_id', sql.Int, req.body.row_id )
+                .input('editRecord', sql.NVarChar(sql.MAX), req.body.editRecord )
+                .execute('spChoferesUpdate', (err, result) => {
+                    logToFile("Request:  " + req.originalUrl)
+                    logToFile("Request:  " + JSON.stringify(req.body))
+                    logToFile("Perf spChoferesUpdate:  " + ((new Date() - start) / 1000) + ' secs' )
+
+                    if(err){
+                        logToFile("DB Error:  " + err.procName)
+                        logToFile("Error:  " + JSON.stringify(err.originalError.info))
+                        res.status(400).send(err.originalError);
+                        return;
+                    }
+                    res.setHeader('content-type', 'application/json');
+                    res.status(200).send(result.recordset);
+                })
+            }catch(ex){
+                logToFile("Service Error")
+                logToFile(ex)
+                res.status(400).send(ex);
+                return;
+            }
+        }
+    })
+})
+//#endregion CHOFER
 
 //#region CHART_ACCOUNTS
 app.get(process.env.iisVirtualPath+'spAccAccountsSelectEdit', veryfyToken, function(req, res) {
